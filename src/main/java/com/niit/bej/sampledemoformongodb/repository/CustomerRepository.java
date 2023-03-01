78d757485da6a25b4ae8bc8f07dc333874c465db
r.mythreye@niit.com <r.mythreye@niit.com>
2023-03-01 09:48:07 +0530

    Missing Annotations added

--- src/main/java/com/niit/bej/sampledemoformongodb/repository/CustomerRepository.java
+++ src/main/java/com/niit/bej/sampledemoformongodb/repository/CustomerRepository.java
@@ -2,6 +2,8 @@ package com.niit.bej.sampledemoformongodb.repository;
 
 import com.niit.bej.sampledemoformongodb.domain.Customer;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CustomerRepository extends MongoRepository<Customer, Integer> {
 }
--- src/main/java/com/niit/bej/sampledemoformongodb/service/CustmoerServiceImpl.java
+++ src/main/java/com/niit/bej/sampledemoformongodb/service/CustmoerServiceImpl.java
@@ -2,11 +2,13 @@ package com.niit.bej.sampledemoformongodb.service;
 
 import com.niit.bej.sampledemoformongodb.domain.Customer;
 import com.niit.bej.sampledemoformongodb.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class CustmoerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
 
+    @Autowired
     public CustmoerServiceImpl(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
