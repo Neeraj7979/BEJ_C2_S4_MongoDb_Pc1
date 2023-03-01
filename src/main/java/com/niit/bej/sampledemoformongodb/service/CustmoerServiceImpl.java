1cd76e8eef4b0289d7e8e3ecdeb3df04b333e023
r.mythreye@niit.com <r.mythreye@niit.com>
2023-03-01 10:04:00 +0530

    @service annotation added

--- src/main/java/com/niit/bej/sampledemoformongodb/service/CustmoerServiceImpl.java
+++ src/main/java/com/niit/bej/sampledemoformongodb/service/CustmoerServiceImpl.java
@@ -3,7 +3,9 @@ package com.niit.bej.sampledemoformongodb.service;
 import com.niit.bej.sampledemoformongodb.domain.Customer;
 import com.niit.bej.sampledemoformongodb.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CustmoerServiceImpl implements CustomerService {
 
     private final CustomerRepository customerRepository;
