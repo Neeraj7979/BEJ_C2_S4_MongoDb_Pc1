c636de084e01444fe431564eddfdaf4a468f1fea
r.mythreye@niit.com <r.mythreye@niit.com>
2023-03-01 09:18:34 +0530

    initial commit

---
+++ src/main/java/com/niit/bej/sampledemoformongodb/SampledemoformongodbApplication.java
@@ -0,0 +1,13 @@
+package com.niit.bej.sampledemoformongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampledemoformongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampledemoformongodbApplication.class, args);
+    }
+
+}
