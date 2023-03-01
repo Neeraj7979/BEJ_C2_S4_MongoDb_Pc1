7fd6297fa8df10d010f868b1ead0910f13f07e70
r.mythreye@niit.com <r.mythreye@niit.com>
2023-03-01 09:44:23 +0530

    Service package created and CustomerService.java added to the same

---
+++ src/main/java/com/niit/bej/sampledemoformongodb/service/CustomerService.java
@@ -0,0 +1,7 @@
+package com.niit.bej.sampledemoformongodb.service;
+
+import com.niit.bej.sampledemoformongodb.domain.Customer;
+
+public interface CustomerService {
+    Customer addCustomer(Customer customer);
+}
